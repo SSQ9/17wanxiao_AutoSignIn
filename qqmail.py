5ca49f1e92a7918ba624f57c1b150190baa7c3fb
SSQ9 <76927823+SSQ9@users.noreply.github.com>
2021-04-25 21:04:07 +0800

    Update qqmail.py

--- qqmail.py
+++ qqmail.py
@@ -43,8 +43,8 @@ for i in range(2):
       d1 = '\r\n\r\n' + '明天'
     else:
       d1 = ''
-    d1 += u'\r\n\r\n咸阳' + '\r' + yb[i]['cond_txt_d'] + '\r' + yb[i]['tmp_min'] + '~' + yb[i]['tmp_max'] + '℃'  + '\r\n'
-    d1 += yb[i]['wind_dir']  + '\r' + yb[i]['wind_sc'] + '级' + '\r\n'
+    d1 += u'\r\n\r\n咸阳' + '\t' + yb[i]['cond_txt_d'] + '\t' + yb[i]['tmp_min'] + '~' + yb[i]['tmp_max'] + '℃'  + '\r\n'
+    d1 += yb[i]['wind_dir']  + '\t' + yb[i]['wind_sc'] + '级' + '\r\n'
     d3 += d1
 #    d2 = gj['txt'] + ' \r\n' + cy['txt']
 #    d3 += d1 + ' \n' + d2
